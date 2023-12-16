f48766c7139a2036c5dc5cfd443af208a88f6f1e
Sergej Dikun <sergej.dikun@gmail.com>
2023-12-16 16:41:50 +0000

    logic unit tests are fully added for the BloodPressure.cs file

--- BPCalculator.UnitTests/UnitTest1.cs
+++ BPCalculator.UnitTests/UnitTest1.cs
@@ -32,6 +32,44 @@ namespace BPCalculator.UnitTests
             Assert.AreEqual(BPCategory.High, result, "Blood pressure should be classified as High.");
         }
 
-        // Additional tests for Low, PreHigh, and Unknown categories
+        [TestMethod]
+        public void Category_ShouldReturnLow_WhenPressureIsLow()
+        {
+            // Arrange
+            var bp = new BloodPressure { Systolic = 80, Diastolic = 50 };
+
+            // Act
+            var result = bp.Category;
+
+            // Assert
+            Assert.AreEqual(BPCategory.Low, result, "Blood pressure should be classified as Low.");
+        }
+
+        [TestMethod]
+        public void Category_ShouldReturnPreHigh_WhenPressureIsPreHigh()
+        {
+            // Arrange
+            var bp = new BloodPressure { Systolic = 130, Diastolic = 85 };
+
+            // Act
+            var result = bp.Category;
+
+            // Assert
+            Assert.AreEqual(BPCategory.PreHigh, result, "Blood pressure should be classified as Pre-High.");
+        }
+
+        [TestMethod]
+        public void Category_ShouldReturnUnknown_WhenPressureIsOutOfRange()
+        {
+            // Arrange
+            var bp = new BloodPressure { Systolic = 200, Diastolic = 110 }; // Example out of range values
+
+            // Act
+            var result = bp.Category;
+
+            // Assert
+            Assert.AreEqual(BPCategory.Unknown, result, "Blood pressure should be classified as Unknown for out of range values.");
+        }
+
     }
 }
