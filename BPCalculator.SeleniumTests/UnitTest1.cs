9d3cb8088003f0ccfc1207121aa718c47d0788a9
Sergej Dikun <sergej.dikun@gmail.com>
2023-12-16 18:59:51 +0000

    fully working selenium tests with tesUrl enabled

--- BPCalculator.SeleniumTests/UnitTest1.cs
+++ BPCalculator.SeleniumTests/UnitTest1.cs
@@ -1,11 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
 namespace BPCalculator.SeleniumTests
 {
     [TestClass]
-    public class UnitTest1
+    public class BloodPressureWebTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private string testUrl;
+
+        [TestInitialize]
+        public void Setup()
         {
+            driver = new ChromeDriver();
+            testUrl = Environment.GetEnvironmentVariable("BPCALCULATOR_BASE_URL") ?? "https://bpcalculatorca.azurewebsites.net/";
+            driver.Navigate().GoToUrl(testUrl);
+        }
+
+        [TestMethod]
+        public void TestBloodPressureCategory()
+        {
+            IWebElement systolicInput = driver.FindElement(By.Id("BP_Systolic"));
+            IWebElement diastolicInput = driver.FindElement(By.Id("BP_Diastolic"));
+            IWebElement submitButton = driver.FindElement(By.XPath("//input[@value='Submit']"));
+
+            systolicInput.Clear();
+            diastolicInput.Clear();
+
+            systolicInput.SendKeys("120");
+            diastolicInput.SendKeys("80");
+            submitButton.Click();
+
+            IWebElement resultDiv = driver.FindElement(By.CssSelector("div.form-group:nth-child(4)"));
+            Assert.IsTrue(resultDiv.Text.Contains("Ideal"), "The blood pressure should be classified as Ideal.");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
